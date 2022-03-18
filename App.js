b8c1f09b8eb36ef1483053769e802e8c89f2f31e
PAULO RICARDO BARRETO MENDES <202003226841@alunos.estacio.br>
2022-03-17 21:06:50 -0300

    hj

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-web';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <text>Estácio</text>
+      <Image source={header} style={style.header} ></Image>
+      <text>Xbox Series x</text>
+      <Image source={sony} style={style.header}></Image>
+      <Image source={uri:https://saude.abril.com.br/wp-content/uploads/2020/05/treino-em-casa.png?w=680}
+      <text>sony</text>
+      <text>Lorem ipsum dolor sit amet conseqt adiscipli elit</text>
+      <text>R$ 4.0000,00</text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  detailsView: {
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    width: 390,
+    height: 300,
+  }
 });
